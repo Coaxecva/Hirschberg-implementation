fddc4ea317fbc4970c5f069aedce170829eaebb7
Coaxecva <tmquang@gmail.com>
2019-04-04 21:58:54 -0500

    add algorithm B

--- LCS.py
+++ LCS.py
@@ -7,8 +7,7 @@ def print_matrix(a, m, n):
         print()
 
 def lcs_algo_A(X, Y, m, n): 
-    L = [[0 for x in range(n+1)] for x in range(m+1)] 
-    
+    L = [[0 for x in range(n+1)] for x in range(m+1)]     
     # Following steps build L[m+1][n+1] in bottom up fashion. Note 
     # that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1]  
     for i in range(1, m+1): 
@@ -20,8 +19,7 @@ def lcs_algo_A(X, Y, m, n):
     return L
     
 def lcs(X, Y, m, n, L):
-    index = L[m][n] 
-  
+    index = L[m][n]   
     # Create a character array to store the lcs string 
     lcs = [""] * (index+1) 
     lcs[index] = "" 
@@ -29,8 +27,7 @@ def lcs(X, Y, m, n, L):
     # one by one store characters in lcs[] 
     i = m 
     j = n 
-    while i > 0 and j > 0: 
-  
+    while i > 0 and j > 0:   
         # If current character in X and Y are same, then 
         # current character is part of LCS 
         if X[i-1] == Y[j-1]: 
@@ -38,7 +35,6 @@ def lcs(X, Y, m, n, L):
             i-=1
             j-=1
             index-=1
-
         # If not same, then find the larger of two and 
         # go in the direction of larger value 
         elif L[i-1][j] > L[i][j-1]: 
@@ -49,6 +45,9 @@ def lcs(X, Y, m, n, L):
     
   
 def lcs_algo_B(X, Y, m, n):
+    if m==0 or n==0:
+        return 0
+    # Initialization
     K = [[0 for i in range(n+1)] for j in range(2)] 
   
     for i in range(1, m+1):
@@ -58,7 +57,46 @@ def lcs_algo_B(X, Y, m, n):
                 K[1][j] = K[0][j-1] + 1
             else:
                 K[1][j] = max(K[1][j-1], K[0][j])
-    return K
+    #return K
+    return K[1][n]
+
+def lcs_algo_C(X, Y, m, n):
+    # X is empty
+    if m == 0:
+        return ""
+    if m == 1:
+        # Check if only 1 character X in Y
+        for c in Y:
+            if c == X[0]:
+                return c
+        return ""
+    # X has more than 1 character
+    ll = lcs_algo_B(X, Y, m, n)
+    l1 = 0
+    l2 = 0
+    # Split X
+    x1 = X[:m//2]
+    x2 = X[m//2:]
+    # Check for correct split of Y
+    kk = -1
+    for k in range(n):
+        l1 = lcs_algo_B(x1, Y[:k], m//2, k)
+        l2 = lcs_algo_B(x2, Y[k:], m-m//2, n-k)
+        if ll == l1+l2:
+            kk = k
+            break
+
+    # Flag!!!!
+    if kk == -1:
+        print("BUG HERE!!!")
+
+    # Split Y
+    y1 = Y[:kk]
+    y2 = Y[kk:]
+    # Solve simpler problems
+    c1 = lcs_algo_C(x1, y1, m//2, kk)
+    c2 = lcs_algo_C(x2, y2, m-m//2, n-kk)
+    return c1+c2
 
 
 if __name__ == '__main__':
@@ -71,14 +109,14 @@ if __name__ == '__main__':
 
     # Algorithm A
     L_A = lcs_algo_A(X, Y, m, n) 
-    print_matrix(L_A, m+1, n+1)
-    print(L_A[m][n])
+    print_matrix(L_A, m+1, n+1)    
     strlcs = lcs(X, Y, m, n, L_A)
-    print("LCS of " + X + " and " + Y + " is " + strlcs)
+    print("LCS (" + X + ", " + Y + ") = " + strlcs)
 
     # Algorithm B
-    L_B = lcs_algo_B(X, Y, m, n)
-    print_matrix(L_B, 2, n+1)
+    #L_B = lcs_algo_B(X, Y, m, n)
+    #print_matrix(L_B, 2, n+1)
 
     # Algorithm C
-    
+    strlcs1 = lcs_algo_C(X, Y, m, n)
+    print("LCS (" + X + ", " + Y + ") = " + strlcs1)    
